f51f68eb5efc7266bf0ded8b05e2830f0f1161a3
Maria Borges <62150120+maria-aparecida-cruz-borges@users.noreply.github.com>
2024-07-14 07:49:12 -0300

    Update queue.test.js

--- queue.test.js
+++ queue.test.js
@@ -8,18 +8,23 @@ describe('Queue', () => {
   })
 
   it('Deve ser capaz de calcular o tamanho da fila', () => {
-    // Seu código aqui
+    const queue = new Queue()
+      expect(queue.size()).toEqual()
   })
 
   it('Deve ser capaz de adicionar um item na fila', () => {
-   // Seu código aqui
+    const queue = new Queue();
+    expect(queue.add('item1')).toEqual(1);
   })
 
   it('Deve escolher o primeiro item da fila', () => {
-    // Seu código aqui
+    const queue = new Queue()
+      expect(queue.peek()).toEqual(0)
   })
 
   it('Deve remover o primeiro item da fila', () => {
-    // Seu código aqui
+    const queue = new Queue();
+      expect(queue.dequeue()).toEqual(0);
+      expect(queue.size()).toEqual(0);
   })
 })
